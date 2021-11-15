48183ed8a408651e8109cb2e80dd27cbf85980e9
Joshua Foschi <64101713+foscjos@users.noreply.github.com>
2021-11-14 18:07:19 -0600

    Update mongo.js - running mongodb locally in docker

--- src/mongo.js
+++ src/mongo.js
@@ -3,7 +3,7 @@ const env = require('./environment/environment');
 
 mongoose.Promise = global.Promise;
 
-const mongoUri = `mongodb://${env.dbName}:${env.key}@${env.dbName}.mongo.cosmos.azure.com:${env.port}/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@readme-main-db@`;
+const mongoUri = `mongodb://localhost:${env.port}`;
 
 function connect() {
     return mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
